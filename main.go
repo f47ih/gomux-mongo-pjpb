4c14a8923db26011af0e056e7a64d5dd7acd925b
Muhammad Fatih <muhammadfatih4994@gmail.com>
2021-03-25 21:08:36 +0700

    still error

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 import "go.mongodb.org/mongo-driver/mongo"
-import "github.com/f47ih/gomux-mongo-pjpb/config"
+import "/gomux-mongo-pjpb/config"
 
 func main() {
 	fmt.Println("Hello")
